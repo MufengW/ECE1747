6e08a6aae9f1500f55e116efd3b2e1370d2a7787
Mufeng Wang <mufeng.wang@mail.utornto.ca>
2023-10-27 21:16:32 -0400

    plot for mode 3

--- A1/test.sh
+++ A1/test.sh
@@ -1,96 +1,24 @@
 #!/bin/bash
 
-# Default values
-thread_count=16
-num_process=1
-max_lines=""
-debug=0
+# Initialize variables
+result_folder="results_mode_3"
+num_process=8
+thread_count=64
 
-display_usage() {
-    echo "Usage: $0 [-t <thread_count>] [-l <max_lines>] [-d]"
-    echo "Options:"
-    echo "  -t <thread_count>   Specify the number of threads (default: 16)"
-    echo "  -l <max_lines>      Specify the maximum number of lines to read from csv"
-    echo "  -d                  Use debug mode for testing"
-    echo "  -h                  Display this help message and exit"
-}
+# Create result folder if it doesn't exist
+mkdir -p $result_folder
 
-while getopts ":t:l:p:dh" opt; do
-  case $opt in
-    t) thread_count="$OPTARG" ;;
-    l) max_lines="$OPTARG" ;;
-    p) num_process="$OPTARG" ;;
-    d) debug=1 ;;
-    h) display_usage; exit 0 ;;
-    *) echo "Invalid option: -$OPTARG" >&2; display_usage; exit 1 ;;
-  esac
+# Data sizes to test. You can modify this array as needed.
+data_sizes=(100 500 1000 5000 10000 50000 100000 500000 1000000)
+
+# Loop through each data size
+echo "Data collection for Mode 3 started."
+for max_lines in "${data_sizes[@]}"; do
+  # Run the program and redirect output to a file
+  mpiexec -np "$num_process" build/A1 3 "$thread_count" "$max_lines" > $result_folder/${max_lines}_3.out 2> /dev/null
+
+  # Extract and save the time for each process
+  grep 'Mode 3 with' $result_folder/${max_lines}_3.out | awk '{print $8}' >> $result_folder/time_${max_lines}.txt
+
 done
-
-# Get the number of CPU cores using lscpu and awk
-num_cores=$(lscpu | awk -F: '/^Core\(s\) per socket:/ {print $2}' | tr -d ' ')
-
-# Check if num_process is greater than num_cores
-if [ $num_process -gt $num_cores ]; then
-  echo "Error: Number of processes ($num_process) is greater than the number of available CPU cores ($num_cores)."
-  exit 1
-fi
-
-result_folder=$HOME/ECE1747_A1_test
-rm -rf $result_folder
-mkdir $result_folder
-
-# Compile for mode 1,2
-if (( debug == 1 )); then
-    echo "Compiling in debug mode..."
-    bash build.sh -m debug
-else
-    echo "Compiling in release mode..."
-    bash build.sh -m release
-fi
-
-if [[ -z "$max_lines" ]]; then
-    build/A1 1 1 > $result_folder/1.out 2> /dev/null
-    build/A1 2 "$thread_count" > $result_folder/2.out 2> /dev/null
-else
-    build/A1 1 1 "$max_lines" > $result_folder/1.out 2> /dev/null
-    build/A1 2 "$thread_count" "$max_lines" > $result_folder/2.out 2> /dev/null
-fi
-
-# Recompile for mode 3
-if (( debug == 1 )); then
-    echo "Compiling in debug mode..."
-    bash build.sh -m debug -p
-else
-    echo "Compiling in release mode..."
-    bash build.sh -m release -p
-fi
-
-if [[ -z "$max_lines" ]]; then
-    mpiexec -np "$num_process" build/A1 3 "$thread_count" > $result_folder/3.out 2> /dev/null
-else
-    mpiexec -np "$num_process" build/A1 3 "$thread_count" "$max_lines" > $result_folder/3.out 2> /dev/null
-fi
-
-# Print the first line of 1.out and 2.out
-head -n 1 $result_folder/1.out
-head -n 1 $result_folder/2.out
-head -n $((2 * num_process)) $result_folder/3.out
-
-# Save the remaining lines to result*.out files
-grep 'Particle .* x = .* y = .* force = .*' $result_folder/1.out | sort -k2 -n > $result_folder/result1.out
-grep 'Particle .* x = .* y = .* force = .*' $result_folder/2.out | sort -k2 -n > $result_folder/result2.out
-grep 'Particle .* x = .* y = .* force = .*' $result_folder/3.out | sort -k2 -n > $result_folder/result3.out
-
-# Testing mode 2
-if diff -q $result_folder/result1.out $result_folder/result2.out >/dev/null; then
-    echo "Mode2 test passed!"
-else
-    echo "Mode2 test failed!"
-fi
-
-# Testing mode 3
-if diff -q $result_folder/result1.out $result_folder/result3.out >/dev/null; then
-    echo "Mode3 test passed!"
-else
-    echo "Mode3 test failed!"
-fi
+echo "Data collection for Mode 3 completed."
